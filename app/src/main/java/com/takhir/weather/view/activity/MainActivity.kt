c8483c920e67978d942e8620496b7ab28f6c67a8
Takhir Nafikov <takhir.learn@gmail.com>
2020-06-18 22:18:56 +0300

    init

---
+++ app/src/main/java/com/takhir/weather/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.takhir.weather.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takhir.weather.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
